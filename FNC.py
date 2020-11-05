0769b648d54c7e191ff7cff0cc8ad07ff81843b5
Feligx <34454204+Feligx@users.noreply.github.com>
2020-11-05 17:35:45 -0500

    Add files via upload

--- FNC.py
+++ FNC.py
@@ -47,12 +47,54 @@ def enFNC(A):
 # Input: A (cadena) en notacion inorder
 # Output: B (cadena), Tseitin
 def Tseitin(A, letrasProposicionalesA):
-    letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
+    #letrasProposicionalesB = [chr(x) for x in range(97, 110)]
+    #letrasProposicionalesB = ['A', 'B', 'C', 'D', 'E', 'F'] # usada para el punto 4
+    #letrasProposicionalesB = [chr(x) for x in range(65, 91)] #usada para el punto 5
+    #letrasProposicionalesB = [chr(x) for x in range(65, 78)] #usada para el punto 6
+    letrasProposicionalesB = [chr(x) for x in range(65, 91)]
+    
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
-
+    L=[]
+    pila=[]
+    I=-1
+    s=A[0]
+    while(len(A)>0):
+        if len(pila) != 0 and pila[-1]=='-' and (s in letrasProposicionalesA or s in letrasProposicionalesB):
+            I+=1
+            Atomo=letrasProposicionalesB[I]
+            pila=pila[:-1]
+            pila.append(Atomo)
+            L.append(Atomo + "=" + "-"+s)
+            A=A[1:]
+            if len(A) > 0:
+                s= A[0]
+        elif s==')':
+            w=pila[-1]
+            u=pila[-2]
+            v=pila[-3]
+            pila=pila[:len(pila)-4]
+            I+=1
+            Atomo=letrasProposicionalesB[I]
+            L.append(Atomo + "=("+v+u+w+")")
+            s=Atomo
+        else:
+            pila.append(s)
+            A=A[1:]
+            if len(A) > 0:
+                s=A[0]
+    B=""
+    if I<0:
+        Atomo = pila[-1]
+    else:
+        Atomo = letrasProposicionalesB[I]
+    for x in L:
+        y= enFNC(x)
+        B+="Y"+y
+    B= Atomo + B
+    return B
     #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
-    pass
-
+    
+    
 # Subrutina Clausula para obtener lista de literales
 # Input: C (cadena) una clausula
 # Output: L (lista), lista de literales
@@ -70,7 +112,6 @@ def Clausula(C):
             L.append(s)
             C=C[2:]
     return L
-    
 
 # Algoritmo para obtencion de forma clausal
 # Input: A (cadena) en notacion inorder en FNC
@@ -78,7 +119,6 @@ def Clausula(C):
 def formaClausal(A):
 
     #  IMPLEMENTAR AQUI ALGORITMO FORMA CLAUSAL
-    
     L=[]
     i=0
     while len(A) >0:
@@ -93,4 +133,3 @@ def formaClausal(A):
             else:
                 i+=1
     return L
-
